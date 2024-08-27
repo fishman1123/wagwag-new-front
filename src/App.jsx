f3473aa2160988366d239ebb30f1ecb35fc7030f
fishman <cho.y.j1997@gmail.com>
2024-08-27 18:33:36 +0900

    type : [#feature] 불필요한 css 파일 삭제 및 구글 로그인 관련 npm 설치, 환경변수 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import { createRoot } from 'react-dom/client';
 import { RouterObject } from './RouterList'; // Import RouterObject instead of RouterList
 import './styles/index.css';
 import { RecoilRoot } from 'recoil';
-// import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-// const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID; // Access your Google Client ID from .env
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID; // Access your Google Client ID from .env
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        {/*<GoogleOAuthProvider clientId={clientId}>*/}
+        <GoogleOAuthProvider clientId={clientId}>
             <RecoilRoot>
                 <RouterProvider router={RouterObject} />
             </RecoilRoot>
-        {/*</GoogleOAuthProvider>*/}
+        </GoogleOAuthProvider>
     </StrictMode>,
 );
