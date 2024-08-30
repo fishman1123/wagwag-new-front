f70e9a7cdd374be322a58ccbb1f5336a9d2149cb
jiyounglee <skfnxh1021@naver.com>
2024-08-30 15:04:05 +0900

    type: [#feature] 보안 라우터 비활성화

--- src/RouterList.jsx
+++ src/RouterList.jsx
@@ -35,9 +35,9 @@ export const RouterList = () => [
     // All other routes are protected
     path: "/",
     element: (
-        <SecuredRoute>
-          <Layout />
-        </SecuredRoute>
+      <SecuredRoute>
+        <Layout />
+      </SecuredRoute>
     ),
     children: [
       {
