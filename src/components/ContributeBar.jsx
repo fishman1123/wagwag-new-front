85a6a4000b95e35c3015d28cb7e992339634fc9b
jueunseuk <junsu120202@gmail.com>
2024-09-01 18:50:29 +0900

    type : [#feature] 마이페이지 구현 완료, overflow 관련 이슈 미해결

--- src/components/ContributeBar.jsx
+++ src/components/ContributeBar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
-const ContributeBar = () => {
-    return (
-        <>
-            <Wrapper>
-                <Bar></Bar>
-            </Wrapper>
-        </>
-    );
-}
+const ContributeBar = ({ contribution }) => {
+  return (
+    <Wrapper>
+      <Bar>
+        <FilledBar contribution={contribution}></FilledBar>
+      </Bar>
+      <Indicator contribution={contribution}></Indicator>
+      <ChasingBox contribution={contribution}>상위&nbsp;<span style={{color: "#57f98e"}}>{contribution+`%`}</span></ChasingBox>
+    </Wrapper>
+  );
+};
 
 export default ContributeBar;
 
@@ -17,13 +19,46 @@ export default ContributeBar;
 // style component
 
 const Wrapper = styled.div`
-    margin-bottom: 3.7vw;
+  margin-bottom: 3.7vw;
 `;
 
 const Bar = styled.div`
-    width: 100%;
-    height: 0.4vw;
-    background-color: #2B2B2B;
+  width: 100%;
+  height: 0.4vw;
+  background-color: #2b2b2b;
+  border-radius: 1vw;
+  position: relative;
+  overflow: hidden;
 `;
 
+const FilledBar = styled.div`
+  width: ${(props) => props.contribution}%;
+  height: 100%;
+  background-color: #57f98e;
+  border-radius: 1vw;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Indicator = styled.div`
+    height: 1.8vw;
+    border-left: 1px dotted #9e9e9e;
+    margin-left: ${(props) => props.contribution}%;
+    margin-bottom: 1px;
+    margin-top: 1px;
+`;
+
+const ChasingBox = styled.div`
+    width: 9.8vw;
+    height: 3.6vw;
+    border: #787878 1px solid;
+    border-radius: 1vw;
+    color: white;
+    font-size: 1.25vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: calc(${(props) => props.contribution}% - 4.9vw);
+`;
 
