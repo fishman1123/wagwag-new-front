7bf6debc2c9d3d68372751eff99c4fe76ee3668f
fishman <cho.y.j1997@gmail.com>
2024-09-05 14:29:36 +0900

    type: [#feature] 메인 최신 머지 후 푸쉬

--- src/components/layout/UploadLayout.jsx
+++ src/components/layout/UploadLayout.jsx
@@ -9,126 +9,8 @@ import HomeIcon from "../../assets/HomeIcon.png";
 import MapIcon from "../../assets/MapIcon.png";
 import SearchIcon from "../../assets/SearchIcon.png";
 import SettingIcon from "../../assets/SettingIcon.png";
-
-
-const LogoImg = styled.img.attrs({
-    src: logoImage,
-    alt: "",
-})`
-    top: 2.9vw;
-    left: 1.3vw;
-    position: absolute;
-    width: 7.7vw;
-    height: 1.3vw;
-    `;
-
-const UserName = styled.h1`
-    font-family: 'Pretendard-Medium';
-    font-size: 0.93vw;
-    position: absolute;
-    width: 10vw;
-    margin-top: 48vw;
-    text-align: center;
-    color: white;
-`;
-
-const UploadIconImg = styled.img.attrs({
-    src: UploadIcon,
-    alt: "",
-})`
-    top: 11.3vw;
-    left: 4.3vw;
-    position: absolute;
-    width: 1.77vw;
-    height: 1.77vw;
-        &:hover {
-        cursor: pointer;
-    }
-    `;
-
-const HomeIconImg = styled.img.attrs({
-    src: HomeIcon,
-    alt: "",
-})`
-    top: 17vw;
-    left: 4.3vw;
-    position: absolute;
-    width: 1.87vw;
-    height: 1.7vw;
-        &:hover {
-        cursor: pointer;
-    }
-    `;
-
-const MapIconImg = styled.img.attrs({
-    src: MapIcon,
-    alt: "",
-})`
-    top: 22.8vw;
-    left: 4.3vw;
-    position: absolute;
-    width: 1.77vw;
-    height: 1.92vw;
-        &:hover {
-        cursor: pointer;
-    }
-    `;
-
-const SearchIconImg = styled.img.attrs({
-    src: SearchIcon,
-    alt: "",
-})`
-    top: 28.7vw;
-    left: 4.3vw;
-    position: absolute;
-    width: 1.77vw;
-    height: 1.77vw;
-        &:hover {
-        cursor: pointer;
-    }
-    `;
-
-const SettingIconImg = styled.img.attrs({
-    src: SettingIcon,
-    alt: "",
-})`
-    top: 34.5vw;
-    left: 4.3vw;
-    position: absolute;
-    width: 1.87vw;
-    height: 1.82vw;
-        &:hover {
-        cursor: pointer;
-    }
-    `;
-
-const ProfileImage = styled.div`
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    margin-top: 46vw;
-    width: 2.91vw;
-    height: 2.91vw;
-    border-radius: 50%;
-    background: ${(props) => (props.image ? `url(${props.image})` : "gray")};
-    background-size: cover;
-    background-position: center;
-    &:hover {
-        cursor: pointer;
-    }
-`;
-
-const Sidebar = styled.div`
-    position: relative;
-    width: 10vw;
-    height: 100vh;
-    background-color: #080808;
-    border-right: 1px solid #2B2B2B;
-    position: fixed;
-    top: 0;
-    left: 0;
-`;
-
+import {SideBarComponent} from "../../styles/SideBar.jsx";
+import {FooterComponent} from "../../styles/Footer.jsx";
 
 
 
@@ -154,17 +36,10 @@ const LoginLayout = () => {
 
     return (
         <SettingBackImages>
-            <Sidebar>
-                <LogoImg />
-                <UploadIconImg />
-                <HomeIconImg />
-                <MapIconImg />
-                <SearchIconImg />
-                <SettingIconImg />
-                <ProfileImage />
-                <UserName>waggle</UserName>
-            </Sidebar>
+            <SideBarComponent />
+
             <Outlet />
+
         </SettingBackImages>
     )
 }
